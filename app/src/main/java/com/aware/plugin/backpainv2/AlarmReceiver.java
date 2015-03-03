47aa82553b3d8746dad62459ace9cf6f4ed0d1a8
Simo Hosio <simo.hosio@ee.oulu.fi>
2015-03-03 14:21:46 +0200

    Updated features.

--- app/src/main/java/com/aware/plugin/backpainv2/AlarmReceiver.java
+++ app/src/main/java/com/aware/plugin/backpainv2/AlarmReceiver.java
@@ -3,6 +3,9 @@ package com.aware.plugin.backpainv2;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.util.Log;
 import android.content.BroadcastReceiver;
 
@@ -24,9 +27,23 @@ public class AlarmReceiver extends BroadcastReceiver {
 
         int q = intent.getIntExtra("qno", -1);
         Log.d(MYTAG, "Got a request to popup q no: " + q);
+        if (q == 1){
+            playNotification(context);
+        }
         Intent queue_esm = new Intent(ESM.ACTION_AWARE_QUEUE_ESM);
         String esmJSON = "[" + Questions.getInstance().getQuestion(q) + "]";
         queue_esm.putExtra(ESM.EXTRA_ESM, esmJSON);
         context.sendBroadcast(queue_esm);
+
+    }
+
+    private void playNotification(Context c) {
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(c, notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
